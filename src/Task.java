0468499f94e0d9d59aedb6e9b9fd44dd59dbee9d
Dmitry <dim4n94@yandex.ru>
2020-04-13 23:01:18 +0300

    update

--- src/Task.java
+++ src/Task.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Task {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Input a number: ");
+        System.out.print("Введите двоичное число: ");
         int num = in.nextInt();
         String str = String.valueOf(num);
         int n = 0;
         for (int i = str.length() - 1, j = 1; i >= 0; i--, j *= 2) {
             n += (str.charAt(i) - '0') * j;
         }
-        System.out.println("\"" + str + "\" -> " + n);
+        System.out.println(str + " -> " + n);
     }
 }
