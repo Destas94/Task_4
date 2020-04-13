5692588b54eb095a9bd34b6abea9d6216297c366
Dmitry <dim4n94@yandex.ru>
2020-04-13 22:53:53 +0300

    update

--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Task {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = in.nextInt();
+        String str = String.valueOf(num);
+        int n = 0;
+        for (int i = str.length() - 1, j = 1; i >= 0; i--, j *= 2) {
+            n += (str.charAt(i) - '0') * j;
+        }
+        System.out.println("\"" + str + "\" -> " + n);
     }
 }
